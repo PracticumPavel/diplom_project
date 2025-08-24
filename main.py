90d557245eec3a516a0ee68cdc052410a98fa86f
ps <practicum.pavel@yandex.ru>
2025-08-24 10:44:54 +0300

    new app

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, Request, Form, Depends
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine
+from models import Base
+import crud
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/")
+async def read_root(request: Request, db: Session = Depends(get_db)):
+    items = crud.get_items(db)
+    return templates.TemplateResponse("index.html", {"request": request, "items": items})
+
+@app.post("/items/")
+async def create_item(
+    request: Request,
+    name: str = Form(...),
+    description: str = Form(...),
+    db: Session = Depends(get_db)
+):
+    crud.create_item(db, name, description)
+    items = crud.get_items(db)
+    return templates.TemplateResponse("index.html", {"request": request, "items": items})
